ff62b766b45bf1f8d5ac31da0c508d07731d5297
Chipzstar <chipzstar.dev@googlemail.com>
2021-10-06 16:23:49 +0100

    add subscription checkout routes

--- index.js
+++ index.js
@@ -8,54 +8,57 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const jobRoutes = require('./routes/jobs');
 const quoteRoutes = require('./routes/quotes');
-const paymentRoutes = require('./routes/payments')
-const stuartRoutes = require('./routes/stuart')
-const gophrRoutes = require('./routes/gophr')
+const paymentRoutes = require('./routes/payments');
+const subscriptionRoutes = require('./routes/subscriptions');
+const stuartRoutes = require('./routes/stuart');
+const gophrRoutes = require('./routes/gophr');
 const port = process.env.PORT || 3001;
 moment.tz.setDefault("Europe/London");
 
 const {validateApiKey} = require("./middleware/auth");
 
 // defining the Express index
-const index = express();
+const app = express();
 const db = require('./models/index');
 const {genJobReference} = require("./helpers");
 
-index.set('port', process.env.PORT || port);
+app.set('port', process.env.PORT || port);
 
 // adding Helmet to enhance your API's security
-index.use(helmet());
+app.use(helmet());
 
 // using bodyParser to parse JSON bodies into JS objects
-index.use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // enabling CORS for all requests
-index.use(cors());
+app.use(cors());
 
 // adding morgan to log HTTP requests
-index.use(morgan('combined'));
+app.use(morgan('combined'));
 
 // add middleware
-// index.use(validateApiKey)
+// app.use(validateApiKey)
 
 // defining an endpoint to return a welcome message
-index.get('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.send("WELCOME TO SECONDS API");
 });
 
 // CORE ROUTES
-index.use('/api/v1/jobs', validateApiKey, jobRoutes);
-index.use('/api/v1/quotes', validateApiKey, quoteRoutes);
+app.use('/api/v1/jobs', validateApiKey, jobRoutes);
+app.use('/api/v1/quotes', validateApiKey, quoteRoutes);
 
 // FLEET PROVIDERS ROUTES + WEBHOOKS
-index.use('/api/v1/stuart', validateApiKey, stuartRoutes);
-index.use('/api/v1/gophr', gophrRoutes);
+app.use('/api/v1/stuart', validateApiKey, stuartRoutes);
+app.use('/api/v1/gophr', gophrRoutes);
 
 // PAYMENTS ROUTES
-index.use('/api/v1/payments', paymentRoutes)
+app.use('/api/v1/payments', paymentRoutes)
+app.use('/api/v1/subscription', subscriptionRoutes)
 
 // starting the server
-index.listen(port, () => {
+app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 	genJobReference()
 });
