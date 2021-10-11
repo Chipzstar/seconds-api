6693ca967ad7856ac7432578a341b323772fcfc2
Chipzstar <chipzstar.dev@googlemail.com>
2021-10-11 10:35:32 +0100

    add gophr callback url to create job payload

--- routes/gophr.js
+++ routes/gophr.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post("/", async (req, res) => {
 	try {
 		console.log(req.body)
-		res.status(req.body)
+		res.status(200).json(req.body)
 	} catch (err) {
 		console.error(err)
 		res.status(400).json({
