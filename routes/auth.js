7617217b4a7e648365b1659ac5313bdf37f3e8ca
Chipzstar <chipzstar.dev@googlemail.com>
2021-10-23 18:31:34 +0100

    add token endpoint to docs

--- routes/auth.js
+++ routes/auth.js
@@ -7,27 +7,27 @@ router.post('/', async (req, res) => {
 	try {
 	    const { email, password } = req.body;
 		const user = await db.User.findOne({email})
-		console.log(user)
-		// decrypt password in db and check if matches input
-		const isMatch = await user.comparePassword(password)
-		isMatch ? res.status(200).json({
-			message: 'SUCCESS',
-			token: user.apiKey
-		}) : res.status(401).json({
-			status: 401,
-			message: "Password is incorrect"
-		})
-	} catch (err) {
-	    console.error(err)
-		if (err.code === 11000) {
+		if (user) {
+			// decrypt password in db and check if matches input
+			const isMatch = await user.comparePassword(password)
+			isMatch ? res.status(200).json({
+				message: 'SUCCESS',
+				token: user.apiKey
+			}) : res.status(401).json({
+				status: 401,
+				message: "Password is incorrect"
+			})
+		} else {
 			res.status(404).json({
 				status: 404,
-				message: "Invalid credentials, no user found with that email address"
+				message: "No user found with that email address"
 			})
 		}
+	} catch (err) {
+	    console.error(err)
 		res.status(400).json({
 			status: 400,
-			message: err.message
+			message: 'Bad request. Email or password is missing'
 		})
 	}
 })
