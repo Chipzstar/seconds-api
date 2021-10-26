47a729a837e7afa7acc978066d64b5607fb9ffe3
Chipzstar <chipzstar.dev@googlemail.com>
2021-10-26 17:33:44 +0100

    webhook testing

--- routes/shopify.js
+++ routes/shopify.js
@@ -1,23 +1,42 @@
-require("dotenv").config();
-const express = require("express");
-const db = require("../models");
+require('dotenv').config();
+const express = require('express');
+const db = require('../models');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
 	try {
-		console.log("-----------------------------")
-		console.log(req.body)
-		console.log("-----------------------------")
-		res.status(200).json({
-			status: "SUCCESS",
-			message: "webhook received"
-		})
+		// filter the request topic and shop domain
+		const topic = req.headers['x-shopify-topic'];
+		const shop = req.headers['x-shopify-shop-domain'];
+		console.table({ topic, shop });
+		if (topic === 'orders/create') {
+			console.log('-----------------------------');
+			console.log("ORDER ID:")
+			console.log(req.body.id);
+			console.log('-----------------------------');
+			const user = await db.User.findOne({ 'shopify.domain': shop });
+			console.log(user);
+			user
+				? res.status(200).json({
+						status: 'SUCCESS',
+						message: 'webhook received',
+				  })
+				: res.status(404).json({
+						status: 'USER_NOT_FOUND',
+						message: `Failed to find a user with shopify domain ${shop}`,
+				  });
+		} else {
+			res.status(400).json({
+				status: 'UNKNOWN_TOPIC',
+				message: `Webhook topic ${topic} is not recognised`,
+			});
+		}
 	} catch (err) {
-		console.error(err)
+		console.error(err);
 		res.status(500).json({
-			error: { ...err }
-		})
+			error: { ...err },
+		});
 	}
-})
+});
 
 module.exports = router;
