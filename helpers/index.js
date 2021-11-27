4aa9db3056c81a27743ffa8950b9880b0aebf486
Chipzstar <chipzstar.dev@googlemail.com>
2021-11-27 15:52:07 +0000

    re-enable SS-quotes

--- helpers/index.js
+++ helpers/index.js
@@ -278,8 +278,8 @@ async function getResultantQuotes(requestBody, vehicleSpecs) {
 		}
 		let gophrQuote = await getGophrQuote(requestBody, vehicleSpecs);
 		QUOTES.push(gophrQuote);
-		/*let streetStreamQuote = await getStreetStreamQuote(requestBody, vehicleSpecs);
-		QUOTES.push(streetStreamQuote);*/
+		let streetStreamQuote = await getStreetStreamQuote(requestBody, vehicleSpecs);
+		QUOTES.push(streetStreamQuote);
 		if (vehicleSpecs.ecofleetVehicle) {
 			let ecoFleetQuote = {
 				...quoteSchema,
