40702bff3bb927f3ee4df784924a3a864ec29322
Chipzstar <chipzstar.dev@googlemail.com>
2022-04-12 23:03:24 +0100

    add await keyword for notification creation

--- services/notification.js
+++ services/notification.js
@@ -4,7 +4,7 @@ const { Notification } = require('@magicbell/core');
 MagicBellClient.configure({ apiKey: process.env.MAGIC_BELL_API_KEY, apiSecret: process.env.MAGIC_BELL_SECRET_KEY });
 
 const sendNotification = async (external_id, title, content) => {
-	const notification = Notification.create({
+	const notification = await Notification.create({
 		title,
 		content,
 		recipients: [{ external_id }]
