bb26ec6f1b53c46c85585071cb3a71cfbe5ba6e2
Chipzstar <chipzstar.dev@googlemail.com>
2022-04-13 12:44:12 +0100

    add try catch block to notfication service

--- services/notification.js
+++ services/notification.js
@@ -1,16 +1,21 @@
-const MagicBellClient = require("@magicbell/core").default;
+const MagicBellClient = require('@magicbell/core').default;
 const { Notification } = require('@magicbell/core');
 
 MagicBellClient.configure({ apiKey: process.env.MAGIC_BELL_API_KEY, apiSecret: process.env.MAGIC_BELL_SECRET_KEY });
 
 const sendNotification = async (external_id, title, content, category) => {
-	const notification = await Notification.create({
-		category,
-		title,
-		content,
-		recipients: [{ external_id }]
-	});
-	console.log(notification);
+	try {
+		const notification = await Notification.create({
+			category,
+			title,
+			content,
+			recipients: [{ external_id }]
+		});
+		console.log(notification);
+	} catch (err) {
+		console.error(err);
+		throw err
+	}
 };
 
 module.exports = sendNotification;
