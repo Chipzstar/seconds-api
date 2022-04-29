d68d4f0fb0ee5c1a21aa0dc187ad85a1dbabe804
Chipzstar <chipzstar.dev@googlemail.com>
2022-04-29 17:47:49 +0100

    surround email service with try catch block

--- services/email.js
+++ services/email.js
@@ -7,17 +7,21 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = async (options, canSend=true) => {
-	const msg = {
-		from: 'Seconds Technologies <ola@useseconds.com>',
-		to: `${options.name} <${options.email}>`,
-		subject: options.subject,
-		...(options.text && { text: options.text }),
-		...(options.html && { html: options.html }),
-		...(options.templateId && { templateId: options.templateId }),
-		...(options.templateData && { dynamicTemplateData: options.templateData })
-	};
-	process.env.SENDGRID_STATUS === 'active' && canSend && await sgMail.send(msg);
+const sendEmail = async (options, canSend = true) => {
+	try {
+		const msg = {
+			from: 'Seconds Technologies <ola@useseconds.com>',
+			to: `${options.name} <${options.email}>`,
+			subject: options.subject,
+			...(options.text && { text: options.text }),
+			...(options.html && { html: options.html }),
+			...(options.templateId && { templateId: options.templateId }),
+			...(options.templateData && { dynamicTemplateData: options.templateData })
+		};
+		process.env.SENDGRID_STATUS === 'active' && canSend && await sgMail.send(msg);
+	} catch (err) {
+		throw err;
+	}
 };
 
 module.exports = sendEmail;
